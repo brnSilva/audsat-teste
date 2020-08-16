f983cc7fa9d1ebfe0767da9103d36619ba46d142
bru_n <bru_n@192.168.0.108>
2020-08-16 01:57:50 -0300

    primeiro commit com configuração do swagger e configuração basica

---
+++ src/test/java/br/com/audsat/AudsatTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.audsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudsatTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
