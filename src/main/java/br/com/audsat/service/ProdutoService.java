fe6e3862688d5ca40c3e1dc6b434fa08569f40bf
Bruno Batista da Silva <bruno_bsilva@yahoo.com.br>
2020-09-04 02:46:51 -0300

    correção

--- src/main/java/br/com/audsat/service/ProdutoService.java
+++ src/main/java/br/com/audsat/service/ProdutoService.java
@@ -3,8 +3,9 @@ package br.com.audsat.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.audsat.components.ComDescontoComponent;
+import br.com.audsat.components.SemDescontoComponent;
 import br.com.audsat.dto.ProdutoDTO;
-import br.com.audsat.enums.TipoCalculoEnum;
 import br.com.audsat.interfaces.CalculaValorInterface;
 import br.com.audsat.utils.ValoresPropertiesUtil;
 
@@ -14,23 +15,18 @@ public class ProdutoService {
 	@Autowired
 	private ValoresPropertiesUtil valoresPropertiesUtil;
 	
-	private TipoCalculoEnum tipoCalculo;
-	
 	private CalculaValorInterface calculaValor;
 	
 	public ProdutoDTO obterValor(Double valor) {
 		
 		if(valor >= valoresPropertiesUtil.getValorDesconto()) {
 			
-			tipoCalculo = TipoCalculoEnum.values()[0];
+			calculaValor = new ComDescontoComponent();
 			
 		} else {
 			
-			tipoCalculo = TipoCalculoEnum.values()[1];
-
+			calculaValor = new SemDescontoComponent();
 		}
-		
-		calculaValor = tipoCalculo.obterTipoCalculo();
 
 		return calculaValor.desconto(valor, valoresPropertiesUtil);
 	}
